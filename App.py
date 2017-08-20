75895de4b3913b65e0d18015ef82cfbc2be607cd
Ben McChesney <ben.mcchesney@gmail.com>
2017-08-20 13:28:13 -0700

    Prototype working
    Data pushed into DataFrame and exported to JSON
    JSON being loaded by powerBI
    powerBI report with visualizations complete

--- App.py
+++ App.py
@@ -1,8 +1,9 @@
 #made by Ben McChesney 8/19/2017
-#following original tutorial here: https://www.dataquest.io/blog/web-scraping-tutorial-python/
+#based on this https://www.dataquest.io/blog/web-scraping-tutorial-python/
 
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 page = requests.get("http://apps.mercurynews.com/policeguns/")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -10,53 +11,67 @@ soup = BeautifulSoup(page.content, 'html.parser')
 gunItems = soup.find_all( class_ = "itemdata" )
 print( "there are " + str( len( gunItems ) ) + " tags matching itemdata")
 
+serials = list()
+dates = list()
+locations = list()
+types = list()
+makes = list()
+models = list()
+calibers = list()
+reportedStatuses = list()
+vehicles = list()
+recoveredStatuses = list()
+
 it = 0
 for item in gunItems:
     #extract the serial
     serialHeader = item.find( class_ = "serialno monosp")
-    serial = item.find( 'strong' ).get_text()
-    #print( "#" + str( it ) + " serial : " + str( serial ) )
+    serials.append( item.find( 'strong' ).get_text() )
 
     subgroups = item.find_all( 'div' , class_ = 'subgroup' )
     dateAndLocation = subgroups[0].find_all( 'span')
-    date = dateAndLocation[0].get_text()
-    location = dateAndLocation[1].get_text()
+    dates.append( dateAndLocation[0].get_text() )
+    locations.append( dateAndLocation[1].get_text() )
     #print( "#" + str( it ) + " date : " + date + " - location " + location )
 
     gunInfo = subgroups[1]
     gunFields = gunInfo.find_all( 'p' )
-    type = gunFields[0].get_text()
-    make = gunFields[1].get_text()
-    model = gunFields[2].get_text()
-    caliber = gunFields[3].get_text()
+    types.append( gunFields[0].get_text()[6:] )
+    makes.append( gunFields[1].get_text()[6:] )
+    models.append( gunFields[2].get_text()[7:] )
+    calibers.append( gunFields[3].get_text()[9:] )
     #print( "#" + str( it ) + type + " | " + make  + " | " + model  + " | " + caliber )
 
     crimeInfo = subgroups[2].find_all( 'p' )
 
     #substring out the extra characters since these are formatted a little differently
-    reported = crimeInfo[ len( crimeInfo ) - 2 ].get_text()[10:]
-    vehicle = crimeInfo[ len( crimeInfo ) - 1 ].get_text()[14:]
+    reportedStatuses.append( crimeInfo[ len( crimeInfo ) - 2 ].get_text()[10:] )
+    vehicles.append( crimeInfo[ len( crimeInfo ) - 1 ].get_text()[14:] )
 
     recovered = False
     if( len( crimeInfo ) > 2 ):
         recovered = True
 
-    print( "#" + str( it ) +" : " + str( recovered ) + " | " + vehicle  + " | " + reported )
-
-    #reported = crimeInfoo
-
-    '''
-    not recovered 
-    <div class="subgroup">
-        <p><strong>Reported</strong>: Stolen</p>
-        <p><strong>From vehicle?</strong>: Yes</p>
-    </div>
-    
-    recovered TAGS
-    <div class="subgroup">
-        <p class="recovered"><strong>RECOVERED</strong></p>
-        <p><strong>Reported</strong>: Stolen</p>
-        <p><strong>From vehicle?</strong>: Yes</p>
-    </div>
-    '''
+    recoveredStatuses.append( recovered )
     it += 1
+
+print( "end of loop")
+dataFrame = pd.DataFrame({
+    "serial":serials,
+    "date":dates,
+    "location":locations,
+    "gun-type":types,
+    "gun-make":makes,
+    "gun-models":models,
+    "gun-calibers":calibers,
+    "from-vehicle":vehicles,
+    "reported":reportedStatuses,
+    "recovered":recoveredStatuses
+})
+
+print( dataFrame.head( 5 ))
+
+# source : https://stackoverflow.com/questions/39257147/convert-pandas-dataframe-to-json-format
+out = dataFrame.to_json( orient='records', lines=False ).replace('} {', '},{' )
+with open( 'export.json' , 'w' ) as f:
+    f.write( out )
